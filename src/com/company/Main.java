d0e3c15c6df61e068bd33565f5b56d29cb066c29
FoxSe <foxrodion2002@gmail.com>
2022-03-11 20:11:02 +0300

    start commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,28 @@
 package com.company;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
-public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Rodion");
+    public class Main {
+
+        public static void main(String[] args) {
+        BufferedReader objReader = null;
+        try {
+            String strCurrentLine;
+            objReader = new BufferedReader(new FileReader("C:\\Users\\FoxSe\\OneDrive\\Рабочий стол\\Старое\\Студенты.txt"));
+            while ((strCurrentLine = objReader.readLine()) != null) {
+                System.out.println(strCurrentLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (objReader != null)
+                    objReader.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 }
