909ec2f383aae1286ed27e87a93d94a42c948f53
FoxSe <foxrodion2002@gmail.com>
2022-03-13 21:23:12 +0300

    Refactoring.

--- src/main/java/com/company/MeReader.java
+++ src/main/java/com/company/MeReader.java
@@ -3,8 +3,10 @@ package com.company;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class MeReader {
     public void read(String filename) {
@@ -12,12 +14,18 @@ public class MeReader {
         try {
             String strCurrentLine;
             boolean isNum = false; //Признак начала чтения числа в строке
-            Integer intNum; // Прочитаное число тип Integer
+            int intNum; // Прочитаное число тип Integer
             String strNum = "a"; // Прочитаное число тип String
-            List<String> listA = new ArrayList<String>(); // Объявление списка A
-            List<String> listB = new ArrayList<String>(); // Объявление списка B
+            List<String> listAllNumbers = new ArrayList<>(); // Объявление списка всех чисел
+            List<String> listOddNumbers = new ArrayList<>(); // Объявление списка нечетных чисел
             ClassLoader cl = this.getClass().getClassLoader();
-            objReader = new BufferedReader(new FileReader(cl.getResource(filename).getFile())); // Открытие текстового файла
+            URL resource = cl.getResource(filename);
+            if (resource == null){
+                System.out.println("Данный файл не существует!");
+                return;
+            }
+
+            objReader = new BufferedReader(new FileReader(resource.getFile())); // Открытие текстового файла
             while ((strCurrentLine = objReader.readLine()) != null) { // Чтение строки
                 System.out.println(strCurrentLine); // Печать строки
                 for (int i = 0; i < strCurrentLine.length(); ++i) {
@@ -26,31 +34,31 @@ public class MeReader {
                             strNum = Character.toString(strCurrentLine.charAt(i));
                             isNum = true;
                         } else {
-                            strNum = strNum + Character.toString(strCurrentLine.charAt(i));
+                            strNum = strNum + strCurrentLine.charAt(i);
                         }
                     }
                     else {
                         if (isNum){
                             isNum = false;
-                            listA.add(strNum); //добавление элемента в список A
-                            intNum = Integer.valueOf(strNum);
+                            listAllNumbers.add(strNum); //добавление элемента в список A
+                            intNum = Integer.parseInt(strNum);
                             if ((intNum % 2) == 1 ){
-                                listB.add(strNum); //добавление элемента в список B
+                                listOddNumbers.add(strNum); //добавление элемента в список B
                             }
                         }
                     }
                 }
-                if (isNum == true){
+                if (isNum){
                     isNum = false;
-                    listA.add(strNum); //добавление элемента в список A
-                    intNum = Integer.valueOf(strNum);
+                    listAllNumbers.add(strNum); //добавление элемента в список A
+                    intNum = Integer.parseInt(strNum);
                     if ((intNum % 2) == 1 ){
-                        listB.add(strNum); //добавление элемента в список B
+                        listOddNumbers.add(strNum); //добавление элемента в список B
                     }
                 }
             }
-            System.out.println(listA); //Печать списка A
-            System.out.println(listB); //Печать списка B
+            System.out.println(listAllNumbers); //Печать списка A
+            System.out.println(listOddNumbers); //Печать списка B
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
